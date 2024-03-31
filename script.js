c03d159dff6723a5d52bb67e622d8c01c54fa79e
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-03-31 16:40:17 +0530

    topic modules

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("It's Node.js");
+//* >>> first code in node 
+
+// console.log("It's Node.js");
+
+//* >>> modules and how to import them
+//* > modules are reusable block of code that encapsulates related functionality 
+
+// const mathJs = require("./math.js"); //* > for importing modules
+
+// console.log(mathJs.add(2, 1));
+// console.log(mathJs.sub(2, 1));
+
+//* > also write like this 
+
+const { add, sub } = require("./math.js");
+
+console.log(add(2, 1));
+console.log(sub(2, 1));
