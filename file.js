09b0678bbd294b04d8e280481b46966b6675cb22
RUSHI PANCHAL <133478781+rushi-001@users.noreply.github.com>
2024-04-03 14:45:35 +0530

    start fs

---
+++ file.js
@@ -0,0 +1,8 @@
+//* >>> file system module 
+const fs = require("fs");
+
+//* >>> Sync... 
+// fs.writeFileSync("./test.txt", "Hello node sync");
+
+//* >>> Async
+// fs.writeFile("./test.txt", "hello node async", (err) => {});
