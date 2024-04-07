fa9245943004a4bf665552abbad818688a6609ad
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-04-07 23:34:00 +0530

    started extress

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-const http = require("http")
-const fs = require("fs")
-const url = require("url")
+// const http = require("http")
+// const fs = require("fs")
+// const url = require("url")
+
 
 //* >>> https://www.youtube.com/results?search_query=tic+tac+toe
 //* >>> https--{protocol(hyper text trasphar protocol)}--://--www.youtube.com--{domain}--/results--{path}--?--{query starts}--search_query=tic+tac+toe--{query}
 
 //* >>> dependencies in package.json are Important, it store all the node modules info (when you run --npm i-- it look the dependencies and install )
 
-const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} ${req.method} New Req Received\n`;
-    const myUrl = url.parse(req.url, true);
+// const myServer = http.createServer((req, res) => {
+//     const log = `${Date.now()}: ${req.url} ${req.method} New Req Received\n`;
+//     const myUrl = url.parse(req.url, true);
 
-    fs.appendFile("log.txt", log, (err, data) => {
-        switch (myUrl.pathname) {
-            case "/":
-                res.end("home page");
-                break;
-            case "/about":
-                const name = myUrl.query.userName;
-                res.end(`hi, ${name}`);
-                break;
-            default:
-                res.end("404 not found");
-        }
-    })
-});
+//     fs.appendFile("log.txt", log, (err, data) => {
+//         switch (myUrl.pathname) {
+//             case "/":
+//                 res.end("home page");
+//                 break;
+//             case "/about":
+//                 const name = myUrl.query.userName;
+//                 res.end(`hi, ${name}`);
+//                 break;
+//             default:
+//                 res.end("404 not found");
+//         }
+//     })
+// });
 
-myServer.listen(8000, () => { console.log("Server Started!") });
+// myServer.listen(8000, () => { console.log("Server Started!") });
 
+//* >> before express framework........ 
 
+//* >>> Express is an framework for Node.js like React.js is an framework for JavaScript
+
+//* >> after express framework........ 
+
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    return res.send("hello from server");
+})
+
+app.get("/about", (req, res) => {
+    return res.send(`hello, ${req.query.name}`);
+})
+
+app.listen(8000, () => console.log("server started...."));
